24b8d0d91163183c3bfdfa4ccaebcdd14d10b76b
Codeginny <viir.ponce@gmail.com>
2026-04-08 01:15:30 -0300

    TP1: Estructura base, Interfaz Animal y Clases (Puntos 1, 2 y 3)

---
+++ tp1/index.ts
@@ -0,0 +1,39 @@
+interface Animal {
+    nombre: string;
+    hacerSonido(): void;
+}
+
+class Perro implements Animal {
+    constructor(public nombre: string) {}
+
+    hacerSonido(): void {
+        console.log("¡Guau! ¡Guau!");
+    }
+}
+
+class Gato implements Animal {
+    constructor(public nombre: string) {}
+
+    hacerSonido(): void {
+        console.log("¡Miau!");
+    }
+}
+
+class Vaca implements Animal {
+    constructor(public nombre: string) {}
+
+    hacerSonido(): void {
+        console.log("¡Muuu!");
+    }
+}
+
+function describirAnimal(animal: Animal): void {
+    console.log(`Este animal se llama: ${animal.nombre}`);
+    animal.hacerSonido();
+}
+
+const miPerro = new Perro("Firulais");
+const miGato = new Gato("Michi");
+
+describirAnimal(miPerro);
+describirAnimal(miGato);
